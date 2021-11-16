5853042c44a8d8cf779662081905fca3618ec0b2
unknown <zubairjava123@gmail.com>
2021-11-17 01:41:35 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,80 @@
+////////////////Q1//////////
+
+// var now = new Date()
+// document.write(now)
+
+
+// ///////////Q2//////////
+
+
+// function foo(){
+//     var firstName = "Muhammad"
+//     var lastName = "Zubair"
+    
+//      firstName = prompt("Enter your name")
+
+//      document.write(firstName)
+     
+// }
+
+
+// foo()
+
+
+/////////Q3/////////
+
+
+// function num(num1 , num2){
+//       alert(num1 + num2)
+// }
+// num(50 ,30)
+
+
+// ////////////////Q4////////////////
+
+// function number(n1 , n2){
+//          return n1 + n2 + "Addition"
+// }
+
+// var result = number( 50 , 20)
+
+// document.write( number ( 50 , 20)  );
+
+
+
+// ////////////Q5//////////
+
+// function names(){
+
+//     for(var i = 0 ; i<=10 ; i++ ){
+//         document.write("3 x " + " " + i + " " + "=" + " " + 3*i + "<br>") 
+//     }
+// }
+
+// names()
+
+
+
+////////////////Q6//////////////////
+
+// function whether(){
+
+//     var parameter = "the quick brown fox"
+
+//     var capword = parameter.toUpperCase()
+
+//     console.log(capword)
+// }
+
+// whether()
+
+
+// /////////////////Q7/////////////////
+
+// function longest(){
+//     var arr2 = ["Web" , "Development" , "Toturial"]
+//     var result = arr2.slice(0 , 2)
+//     console.log(result)
+// }
+
+// longest()
